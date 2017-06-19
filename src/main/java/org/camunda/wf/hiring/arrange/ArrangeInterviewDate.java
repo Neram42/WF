7aadd17e2ef82ec6caa9fc81627deedb45e07249
Nearm42 <manore92@gmx.de>
2017-06-19 21:45:26 +0200

    initialize git project

---
+++ src/main/java/org/camunda/wf/hiring/arrange/ArrangeInterviewDate.java
@@ -0,0 +1,8 @@
+package org.camunda.wf.hiring.arrange;
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("Loan Approval App")
+public class ArrangeInterviewDate {
+
+}
