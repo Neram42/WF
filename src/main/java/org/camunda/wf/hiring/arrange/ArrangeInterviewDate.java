0d946774da1b9575aba05427d478f580f48b6683
tetee <theresa-sollert@web.de>
2017-06-29 13:27:38 +0200

    commit theresa

--- src/main/java/org/camunda/wf/hiring/arrange/ArrangeInterviewDate.java
+++ src/main/java/org/camunda/wf/hiring/arrange/ArrangeInterviewDate.java
@@ -1,5 +1,45 @@
 package org.camunda.wf.hiring.arrange;
 
+import microsoft.exchange.webservices.data.core.ExchangeService;
+import microsoft.exchange.webservices.data.core.enumeration.misc.ExchangeVersion;
+import microsoft.exchange.webservices.data.core.service.item.EmailMessage;
+import microsoft.exchange.webservices.data.credential.ExchangeCredentials;
+import microsoft.exchange.webservices.data.credential.WebCredentials;
+import microsoft.exchange.webservices.data.property.complex.MessageBody;
+
+import java.net.URI;
+
+import microsoft.exchange.webservices.data.*;
+import microsoft.exchange.webservices.data.autodiscover.exception.AutodiscoverRemoteException;
+
 public class ArrangeInterviewDate {
 
+	
+	 public static void main(String[] args) {
+		 ExchangeService service = new ExchangeService(ExchangeVersion.Exchange2010_SP2);
+		 ExchangeCredentials credentials = new WebCredentials("hr_employee@outlook.de", "HRemployee");
+		 service.setCredentials(credentials);
+		 try {
+			//service.autodiscoverUrl("hr_employee@outlook.de");
+			 service.setUrl(new URI("https://outlook.com/EWS/Exchange.asmx"));
+		} catch (Exception e) {
+			System.out.print("klappt nicht");
+			e.printStackTrace();
+		}
+		 
+		 
+		 EmailMessage msg;
+		try {
+			msg = new EmailMessage(service);
+			 msg.setSubject("Hello world!");
+			 msg.setBody(MessageBody.getMessageBodyFromText("Sent using the EWS Java API."));
+			 msg.getToRecipients().add("t_soll03@uni-muenster.de");
+			 msg.send();
+			 System.out.println("Mail gesendet");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	 }
+	
 }
