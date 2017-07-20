ae8bce9fcd9c113ac2f6774f16f54b206477a24a
Niklas <bpc@niklastewes.de>
2017-07-20 13:43:25 +0200

    ApprovalReceivedServlet Stub

---
+++ src/main/java/org/camunda/wf/hiring/servlets/SendPaymentServlet.java
@@ -0,0 +1,19 @@
+package org.camunda.wf.hiring.servlets;
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "SendPayment", urlPatterns = {"/SendPayment"})
+public class SendPaymentServlet  extends HttpServlet{
+
+	// NOT USED
+	
+}
+
+
+
