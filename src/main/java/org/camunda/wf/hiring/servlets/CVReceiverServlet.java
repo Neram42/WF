da291fd91e135a64d972fa6949d3b3c38edb3658
Niklas <bpc@niklastewes.de>
2017-07-20 17:38:27 +0200

    Konzept: JSON empfangen, JSON in Java Object verwandeln und dann in $scope laden mit Angular

--- src/main/java/org/camunda/wf/hiring/servlets/CVReceiverServlet.java
+++ src/main/java/org/camunda/wf/hiring/servlets/CVReceiverServlet.java
@@ -21,6 +21,8 @@ import org.camunda.bpm.engine.RuntimeService;
 import org.camunda.bpm.engine.impl.util.json.JSONException;
 import org.camunda.bpm.engine.impl.util.json.JSONObject;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.camunda.bpm.engine.variable.Variables;
+import org.camunda.bpm.engine.variable.value.ObjectValue;
 import org.camunda.bpm.model.bpmn.builder.IntermediateCatchEventBuilder;
 import com.fasterxml.jackson.core.*;
 import com.fasterxml.jackson.databind.JsonNode;
@@ -37,16 +39,33 @@ public class CVReceiverServlet extends HttpServlet  {
 		ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
 		RuntimeService runtimeService = processEngine.getRuntimeService();
 		ProcessInstance processInstance;
-		ObjectMapper objectMapper = new ObjectMapper();
 		
+		request.setCharacterEncoding("UTF-8");
+		String contentType = request.getContentType();
+		response.setContentType("application/json");
+		if (!"application/json".equals(contentType)) {
+			response.getWriter().append("{\"error\":\"invalidRequest\",\"status\":\"wrong content type\"}");
+		}
+		
+		ObjectMapper objectMapper = new ObjectMapper();
 		BufferedReader reader = request.getReader();
 		CV cv = objectMapper.readValue(reader, CV.class);
 		
-		String cvString = cv.toString();
-		
+		if (null == cv) {
+			response.getWriter().append("{\"error\":\"invalidRequest\", \"status\":\"GSON not correctly created\"}");
+			return; //break
+		} else {
+			
+			processInstance = runtimeService.startProcessInstanceByMessage("CV"/*, map*/);
+			
+			ObjectValue typedJobInquiry = Variables.objectValue(cv).serializationDataFormat("application/json").create();
+
+			runtimeService.setVariable(processInstance.getId(), "jobInquiry", typedJobInquiry);
+			response.getWriter().append(objectMapper.writeValueAsString(cv));
+		}
 		
 		// build HTML output
-		PrintWriter out = response.getWriter();
+		/*PrintWriter out = response.getWriter();
 		response.setContentType("text/html");
 		out.println("<html><body>");
 		String id = request.getParameter("id");
@@ -60,11 +79,145 @@ public class CVReceiverServlet extends HttpServlet  {
 			out.println("<h2>Error</h2><p>No correlating process instance.</p><p>" + id + "</p>");
 			} }
 			out.println("</body></html>");
-	    out.close();
+	    out.close();*/
 	  }
 }
 
+class CV {
+	private String name;
+	private String address;
+	private String email;
+	private String tel;
+	private Calendar birthday;
+	private String study;
+	private String degree;
+	private String grade;
+	private String sectors;
+	private String motivation;
+	private String skills;
+	private String experience;
+	private Boolean isAccepted;
+	private String rating;
 
+	public CV() {
+		this.name = "Peter Pan";
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public Calendar getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(Calendar birthday) {
+		this.birthday = birthday;
+	}
+
+	public String getStudy() {
+		return study;
+	}
+
+	public void setStudy(String study) {
+		this.study = study;
+	}
+
+	public String getDegree() {
+		return degree;
+	}
+
+	public void setDegree(String degree) {
+		this.degree = degree;
+	}
+
+	public String getGrade() {
+		return grade;
+	}
+
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+
+	public String getSectors() {
+		return sectors;
+	}
+
+	public void setSectors(String sectors) {
+		this.sectors = sectors;
+	}
+
+	public String getMotivation() {
+		return motivation;
+	}
+
+	public void setMotivation(String motivation) {
+		this.motivation = motivation;
+	}
+
+	public String getSkills() {
+		return skills;
+	}
+
+	public void setSkills(String skills) {
+		this.skills = skills;
+	}
+
+	public String getExperience() {
+		return experience;
+	}
+
+	public void setExperience(String experience) {
+		this.experience = experience;
+	}
+	
+	public Boolean getIsAccepted() {
+		return isAccepted;
+	}
+
+	public void setIsAccepted(Boolean isAccepted) {
+		this.isAccepted = isAccepted;
+	}
+
+	public String getRating() {
+		return rating;
+	}
+
+	public void setRating(String rating) {
+		this.rating = rating;
+	}
+}
+
+
+/*
 class CV {
 
     private String instanceID;
@@ -210,3 +363,4 @@ class CV {
 	}
 
 }
+*/
