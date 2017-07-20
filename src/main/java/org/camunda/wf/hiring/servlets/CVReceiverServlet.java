a1fa5a1a295501b87abd8050cc5983b5445361b6
Niklas <bpc@niklastewes.de>
2017-07-20 14:49:25 +0200

    added jackson to dependencies

--- src/main/java/org/camunda/wf/hiring/servlets/CVReceiverServlet.java
+++ src/main/java/org/camunda/wf/hiring/servlets/CVReceiverServlet.java
@@ -1,5 +1,6 @@
 package org.camunda.wf.hiring.servlets;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.HashMap;
@@ -11,10 +12,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.http.protocol.HTTP;
 import org.camunda.bpm.engine.MismatchingMessageCorrelationException;
 import org.camunda.bpm.engine.ProcessEngine;
 import org.camunda.bpm.engine.ProcessEngines;
 import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.impl.util.json.JSONException;
+import org.camunda.bpm.engine.impl.util.json.JSONObject;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.camunda.bpm.model.bpmn.builder.IntermediateCatchEventBuilder;
 
@@ -25,25 +29,39 @@ import org.camunda.bpm.model.bpmn.builder.IntermediateCatchEventBuilder;
 )
 public class CVReceiverServlet extends HttpServlet  {
 	 @SuppressWarnings("deprecation")
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	  {
 		ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
 		RuntimeService runtimeService = processEngine.getRuntimeService();
 		ProcessInstance processInstance;
-		PrintWriter out = response.getWriter();
-		out.println("<html><body>");
+		
 		// 
 		
 		// not finished
-		
+				
 		//
+		
+		
+		  // Work with the data using methods like...
+		  // int someInt = jsonObject.getInt("intParamName");
+		  // String someString = jsonObject.getString("stringParamName");
+		  // JSONObject nestedObj = jsonObject.getJSONObject("nestedObjName");
+		  // JSONArray arr = jsonObject.getJSONArray("arrayParamName");
+		  // etc...
+		
+		
+		
+		
+		PrintWriter out = response.getWriter();
+		out.println("<html><body>");
+	
 		String id = request.getParameter("id");
 		if (null == id) {
 			out.println("<h2>Error</h2><p>Parameter id missing!</p>"); } 
 					else {
 			try {
 			runtimeService.createMessageCorrelation("continueMessage") .processInstanceId(id).correlate();
-			out.println("<h1>Message delivered to process</h1><p>ID: " + id + "</p>"); }
+			out.println("<h1>CVs delivered to process</h1><p>ID: " + id + "</p>"); }
 			catch (MismatchingMessageCorrelationException e) {
 			out.println("<h2>Error</h2><p>No correlating process instance.</p><p>" + id + "</p>");
 			} }
