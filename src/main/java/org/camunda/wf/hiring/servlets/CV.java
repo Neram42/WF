da4a5a8300fd4a9a57bbfba92a15a690acc9a177
Niklas <bpc@niklastewes.de>
2017-07-21 18:38:16 +0200

    CVReceiverServlet now hopefully working, includes sample json object

---
+++ src/main/java/org/camunda/wf/hiring/servlets/CV.java
@@ -0,0 +1,317 @@
+package org.camunda.wf.hiring.servlets;
+
+import java.util.Calendar;
+
+class CV {
+	private String id;
+	private String name;
+	private String address;
+	private String email;
+	private String tel;
+	private Calendar birthday;
+	private String study;
+	private String degree;
+	private String grade;
+	private String sectors;
+	private String motivation;
+	private String skills;
+	private String experience;
+	private Boolean isAccepted;
+	private String rating;
+
+	public CV() {
+		this.name = "Muster";
+	}
+	
+	public CV(String id, String name) {
+		this.name = name;
+		this.setId(id);
+	}
+	// Birthday not included yet
+	public CV(String id, String name, String address, String email, String tel, String study, String degree, 
+			String grade, String sectors, String motivation, String skills, String experience, Boolean isAccepted, String rating) {
+		this.name = name;
+		this.address = address;
+		this.email = email;
+		this.tel = tel;
+		this.study = study;
+		this.degree = degree;
+		this.grade = grade;
+		this.sectors = sectors;
+		this.motivation = motivation;
+		this.skills = skills;
+		this.experience = experience;
+		this.isAccepted = isAccepted;
+		this.rating = rating;
+		this.setId(id);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public Calendar getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(Calendar birthday) {
+		this.birthday = birthday;
+	}
+
+	public String getStudy() {
+		return study;
+	}
+
+	public void setStudy(String study) {
+		this.study = study;
+	}
+
+	public String getDegree() {
+		return degree;
+	}
+
+	public void setDegree(String degree) {
+		this.degree = degree;
+	}
+
+	public String getGrade() {
+		return grade;
+	}
+
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+
+	public String getSectors() {
+		return sectors;
+	}
+
+	public void setSectors(String sectors) {
+		this.sectors = sectors;
+	}
+
+	public String getMotivation() {
+		return motivation;
+	}
+
+	public void setMotivation(String motivation) {
+		this.motivation = motivation;
+	}
+
+	public String getSkills() {
+		return skills;
+	}
+
+	public void setSkills(String skills) {
+		this.skills = skills;
+	}
+
+	public String getExperience() {
+		return experience;
+	}
+
+	public void setExperience(String experience) {
+		this.experience = experience;
+	}
+	
+	public Boolean getIsAccepted() {
+		return isAccepted;
+	}
+
+	public void setIsAccepted(Boolean isAccepted) {
+		this.isAccepted = isAccepted;
+	}
+
+	public String getRating() {
+		return rating;
+	}
+
+	public void setRating(String rating) {
+		this.rating = rating;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+}
+
+
+/*
+class CV {
+
+    private String instanceID;
+    private String name;
+    private String firstname;
+    private String address;
+    private String email;
+    private Calendar birthday;
+    private String study;
+    private String degree;
+    private String grade;
+    private String sectors;
+    private String specialSkills;
+    private String practicalExperience;
+    private int rating;
+    private Calendar receivedOn;
+
+    public CV() {
+        //the famous empty constructor for Jackson
+    }
+
+    public CV(String instanceID,String name, String address,String email,Calendar birthday,String study,String degree,String grade,String sectors,String specialSkills,String practicalExperience,int rating ) {
+      
+    }
+
+	public String getInstanceID() {
+		return instanceID;
+	}
+
+	public void setInstanceID(String instanceID) {
+		this.instanceID = instanceID;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Calendar getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(Calendar birthday) {
+		this.birthday = birthday;
+	}
+
+	public String getStudy() {
+		return study;
+	}
+
+	public void setStudy(String study) {
+		this.study = study;
+	}
+
+	public String getDegree() {
+		return degree;
+	}
+
+	public void setDegree(String degree) {
+		this.degree = degree;
+	}
+
+	public String getGrade() {
+		return grade;
+	}
+
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+
+	public String getSectors() {
+		return sectors;
+	}
+
+	public void setSectors(String sectors) {
+		this.sectors = sectors;
+	}
+
+	public String getSpecialSkills() {
+		return specialSkills;
+	}
+
+	public void setSpecialSkills(String specialSkills) {
+		this.specialSkills = specialSkills;
+	}
+
+	public String getPracticalExperience() {
+		return practicalExperience;
+	}
+
+	public void setPracticalExperience(String practicalExperience) {
+		this.practicalExperience = practicalExperience;
+	}
+
+	public int getRating() {
+		return rating;
+	}
+
+	public void setRating(int raing) {
+		this.rating = raing;
+	}
+
+	public Calendar getReceivedOn() {
+		return receivedOn;
+	}
+
+	public void setReceivedOn(Calendar receivedOn) {
+		this.receivedOn = receivedOn;
+	}
+	
+	@Override
+	public String toString(){
+		StringBuilder sb = new StringBuilder();
+		sb.append("Name="+getName()+"\n");
+		return sb.toString();
+	}
+
+}
+*/
--- src/main/java/org/camunda/wf/hiring/servlets/CVReceiverServlet.java
+++ src/main/java/org/camunda/wf/hiring/servlets/CVReceiverServlet.java
@@ -27,6 +27,7 @@ import org.camunda.bpm.model.bpmn.builder.IntermediateCatchEventBuilder;
 import com.fasterxml.jackson.core.*;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 @WebServlet(
         name = "CVReceiver",
         description = "Servlet for receiving the CVs",
@@ -42,25 +43,26 @@ public class CVReceiverServlet extends HttpServlet  {
 		
 		request.setCharacterEncoding("UTF-8");
 		String contentType = request.getContentType();
-		response.setContentType("application/json");
+		response.setContentType("text/html");
 		if (!"application/json".equals(contentType)) {
 			response.getWriter().append("{\"error\":\"invalidRequest\",\"status\":\"wrong content type\"}");
 		}
 		
 		ObjectMapper objectMapper = new ObjectMapper();
 		BufferedReader reader = request.getReader();
+
 		CV cv = objectMapper.readValue(reader, CV.class);
 		
 		if (null == cv) {
 			response.getWriter().append("{\"error\":\"invalidRequest\", \"status\":\"GSON not correctly created\"}");
 			return; //break
 		} else {
+			response.getWriter().append("Versuche, Daten in den Scope zu schreiben ..." + cv.getId() + " "+ cv.getName());
+			processInstance = runtimeService.startProcessInstanceByMessage("CV"/*, map*/);
 			
-			processInstance = runtimeService.startProcessInstanceByMessage("CVReceiver"/*, map*/);
-			
-			ObjectValue typedJobInquiry = Variables.objectValue(cv).serializationDataFormat("application/json").create();
+			ObjectValue typedCV = Variables.objectValue(cv).serializationDataFormat("application/json").create();
 
-			runtimeService.setVariable(processInstance.getId(), "jobInquiry", typedJobInquiry);
+			runtimeService.setVariable(processInstance.getId(), "jobInquiry", typedCV);
 			response.getWriter().append(objectMapper.writeValueAsString(cv));
 		}
 		
@@ -68,7 +70,7 @@ public class CVReceiverServlet extends HttpServlet  {
 		PrintWriter out = response.getWriter();
 		response.setContentType("text/html");
 		out.println("<html><body>");
-		String id = request.getParameter("id");
+		String id = cv.getId();
 		if (null == id) {
 			out.println("<h2>Error</h2><p>Parameter id missing!</p>" + id); } 
 		else {
@@ -83,284 +85,22 @@ public class CVReceiverServlet extends HttpServlet  {
 	  }
 }
 
-class CV {
-	private String name;
-	private String address;
-	private String email;
-	private String tel;
-	private Calendar birthday;
-	private String study;
-	private String degree;
-	private String grade;
-	private String sectors;
-	private String motivation;
-	private String skills;
-	private String experience;
-	private Boolean isAccepted;
-	private String rating;
+/* Sample JSON Object:
 
-	public CV() {
-		this.name = "Peter Pan";
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getTel() {
-		return tel;
-	}
-
-	public void setTel(String tel) {
-		this.tel = tel;
-	}
-
-	public Calendar getBirthday() {
-		return birthday;
-	}
-
-	public void setBirthday(Calendar birthday) {
-		this.birthday = birthday;
-	}
-
-	public String getStudy() {
-		return study;
-	}
-
-	public void setStudy(String study) {
-		this.study = study;
-	}
-
-	public String getDegree() {
-		return degree;
-	}
-
-	public void setDegree(String degree) {
-		this.degree = degree;
-	}
-
-	public String getGrade() {
-		return grade;
-	}
-
-	public void setGrade(String grade) {
-		this.grade = grade;
-	}
-
-	public String getSectors() {
-		return sectors;
-	}
-
-	public void setSectors(String sectors) {
-		this.sectors = sectors;
-	}
-
-	public String getMotivation() {
-		return motivation;
-	}
-
-	public void setMotivation(String motivation) {
-		this.motivation = motivation;
-	}
-
-	public String getSkills() {
-		return skills;
-	}
-
-	public void setSkills(String skills) {
-		this.skills = skills;
-	}
-
-	public String getExperience() {
-		return experience;
-	}
-
-	public void setExperience(String experience) {
-		this.experience = experience;
-	}
-	
-	public Boolean getIsAccepted() {
-		return isAccepted;
-	}
-
-	public void setIsAccepted(Boolean isAccepted) {
-		this.isAccepted = isAccepted;
-	}
-
-	public String getRating() {
-		return rating;
-	}
-
-	public void setRating(String rating) {
-		this.rating = rating;
-	}
+{
+	"id" : "359acb27-6e2d-11e7-a15a-06b9ccde9c04",
+    "name": "Chuck Norris",
+    "address": "Street 2",
+    "tel": "222333",
+    "study": "IS",
+    "degree": "MSC",
+    "grade": "3",
+    "sectors": "IT and Design",
+    "motivation": "Very motivated",
+    "skills": "Lots of skills",
+    "experience": "Amazing experience",
+    "isAccepted": true,
+    "rating": "exceptional"
 }
-
-
-/*
-class CV {
-
-    private String instanceID;
-    private String name;
-    private String firstname;
-    private String address;
-    private String email;
-    private Calendar birthday;
-    private String study;
-    private String degree;
-    private String grade;
-    private String sectors;
-    private String specialSkills;
-    private String practicalExperience;
-    private int rating;
-    private Calendar receivedOn;
-
-    public CV() {
-        //the famous empty constructor for Jackson
-    }
-
-    public CV(String instanceID,String name, String address,String email,Calendar birthday,String study,String degree,String grade,String sectors,String specialSkills,String practicalExperience,int rating ) {
-      
-    }
-
-	public String getInstanceID() {
-		return instanceID;
-	}
-
-	public void setInstanceID(String instanceID) {
-		this.instanceID = instanceID;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getFirstname() {
-		return firstname;
-	}
-
-	public void setFirstname(String firstname) {
-		this.firstname = firstname;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public Calendar getBirthday() {
-		return birthday;
-	}
-
-	public void setBirthday(Calendar birthday) {
-		this.birthday = birthday;
-	}
-
-	public String getStudy() {
-		return study;
-	}
-
-	public void setStudy(String study) {
-		this.study = study;
-	}
-
-	public String getDegree() {
-		return degree;
-	}
-
-	public void setDegree(String degree) {
-		this.degree = degree;
-	}
-
-	public String getGrade() {
-		return grade;
-	}
-
-	public void setGrade(String grade) {
-		this.grade = grade;
-	}
-
-	public String getSectors() {
-		return sectors;
-	}
-
-	public void setSectors(String sectors) {
-		this.sectors = sectors;
-	}
-
-	public String getSpecialSkills() {
-		return specialSkills;
-	}
-
-	public void setSpecialSkills(String specialSkills) {
-		this.specialSkills = specialSkills;
-	}
-
-	public String getPracticalExperience() {
-		return practicalExperience;
-	}
-
-	public void setPracticalExperience(String practicalExperience) {
-		this.practicalExperience = practicalExperience;
-	}
-
-	public int getRating() {
-		return rating;
-	}
-
-	public void setRating(int raing) {
-		this.rating = raing;
-	}
-
-	public Calendar getReceivedOn() {
-		return receivedOn;
-	}
-
-	public void setReceivedOn(Calendar receivedOn) {
-		this.receivedOn = receivedOn;
-	}
-	
-	@Override
-	public String toString(){
-		StringBuilder sb = new StringBuilder();
-		sb.append("Name="+getName()+"\n");
-		return sb.toString();
-	}
-
-}
-*/
+ 
+ */
