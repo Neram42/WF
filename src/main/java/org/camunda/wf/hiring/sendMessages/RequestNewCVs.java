e0e2c1a742ce59ffc8e2a9a572c60c4a46bcc7aa
Nearm42 <manore92@gmx.de>
2017-07-22 17:04:20 +0200

    added request new CVs

--- src/main/java/org/camunda/wf/hiring/sendMessages/RequestNewCVs.java
+++ src/main/java/org/camunda/wf/hiring/sendMessages/RequestNewCVs.java
@@ -1,5 +1,62 @@
 package org.camunda.wf.hiring.sendMessages;
 
-public class RequestNewCVs {
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+public class RequestNewCVs implements JavaDelegate {
+
+	@Override
+	public void execute(DelegateExecution execution) throws Exception {
+		// build HTTP post with all variables as parameters
+		HttpClient client = HttpClientBuilder.create().build();
+
+		String externalID = (String) execution.getVariable("externalID");
+		String currentDate = (String) execution.getVariable("deadline");
+
+		try {
+			SimpleDateFormat formatter = new SimpleDateFormat("YYYY-MM-DD");
+			Date date = formatter.parse(currentDate);
+			System.out.println(date);
+
+			Date newDate;
+
+			Calendar cal = Calendar.getInstance();
+			cal.setTime(date);
+			cal.add(Calendar.MONTH, 2); 
+			newDate = cal.getTime(); // New date
+
+			String newDuration = formatter.format(newDate);
+
+			String JSON = "{ \"processId\": \"" + externalID + "\",";
+			JSON = JSON + "\"newDuration\": \"" + newDuration + "\"}";
+			String postURL = "http://";
+
+			try {
+				HttpPost post = new HttpPost(postURL);
+				StringEntity postString = new StringEntity(JSON);
+
+				post.setHeader("content-type", "application/json");
+				post.setEntity(postString);
+				client.execute(post);
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
 
 }
