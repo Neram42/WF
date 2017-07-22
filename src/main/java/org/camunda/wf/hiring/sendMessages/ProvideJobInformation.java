a92a443ee4ed0db781187a6616a9eac4626f0ae9
Nearm42 <manore92@gmx.de>
2017-07-22 11:21:35 +0200

    changed Provide Job Info

--- src/main/java/org/camunda/wf/hiring/sendMessages/ProvideJobInformation.java
+++ src/main/java/org/camunda/wf/hiring/sendMessages/ProvideJobInformation.java
@@ -1,55 +1,82 @@
 package org.camunda.wf.hiring.sendMessages;
+
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+
 public class ProvideJobInformation implements JavaDelegate {
-	
-	public void execute(DelegateExecution execution) {
-		//TODO
+
+//	public void execute(DelegateExecution execution) {
+//		// TODO
+//	}
+
+	public void execute(DelegateExecution execution) throws Exception {
+		// build HTTP post with all variables as parameters
+		HttpClient client = HttpClientBuilder.create().build();
+		
+		String id = execution.getProcessInstanceId();
+		String location = (String) execution.getVariable("location");
+		String title = (String) execution.getVariable("title");
+		String requirements = (String) execution.getVariable("requirements");
+		String candidateProfile = (String) execution.getVariable("candidateProfile");
+		String deadline = (String) execution.getVariable("deadline");
+		String tasklist = (String) execution.getVariable("tasklist");
+		
+		
+		String candidateProfileJSON = "[";
+		for (String candidateProfiletemp : candidateProfile.split(",")){
+			candidateProfileJSON = candidateProfileJSON + "\"" + candidateProfiletemp + "\",";
+		}
+		candidateProfileJSON = candidateProfileJSON.substring(0, candidateProfileJSON.length() -1);
+		candidateProfileJSON = candidateProfileJSON + "]";
+		
+		System.out.println(candidateProfileJSON);
+		
+		String tasklistJSON = "[";
+		for (String tasklisttemp : tasklist.split(",")){
+			tasklistJSON = tasklistJSON + "\"" + tasklisttemp + "\",";
+		}
+		tasklistJSON = tasklistJSON.substring(0, tasklistJSON.length() -1);
+		tasklistJSON = tasklistJSON + "]";
+		
+		System.out.println(tasklistJSON);
+		
+		String requirementsJSON = "[";
+		for (String requirementstemp : requirements.split(",")){
+			requirementsJSON = requirementsJSON + "\"" + requirementstemp + "\",";
+		}
+		requirementsJSON = requirementsJSON.substring(0, requirementsJSON.length() -1);
+		requirementsJSON = requirementsJSON + "]";
+		
+		System.out.println(requirementsJSON);
+		
+		String JSON = "{ \"processId\": \"" + id + "\",";
+		JSON = JSON + "\"tile\": \"" + title + "\",";
+		JSON = JSON + "\"location\": \"" + location + "\",";
+		JSON = JSON + "\"requiredGraduation\": \"" + requirementsJSON + "\",";
+		JSON = JSON + "\"candidateProfile\": \"" + candidateProfileJSON + "\",";
+		JSON = JSON + "\"tasklist\": \"" + tasklistJSON + "\",";
+		JSON = JSON + "\"deadline\": \"" + deadline + "\"}";
+
+		System.out.println(JSON);
+		
+
+		String postURL = "http://";
+		
+		try {
+			HttpPost post = new HttpPost(postURL);
+			StringEntity postString = new StringEntity(JSON);
+			
+			post.setHeader("content-type", "application/json");
+			post.setEntity(postString);
+			client.execute(post);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
-    public void executee(DelegateExecution execution) throws Exception {
-        // build HTTP post with all variables as parameters
-        HttpClient client = HttpClientBuilder.create().build();
-//      RequestBuilder requestBuilder = RequestBuilder.get()
-////                .setUri("https://requestb.in/https://requestb.in/1iv9qco1")
-//              .addParameter("id", execution.getProcessInstanceId());
-//      for (String variable : execution.getVariableNames()) {
-//          requestBuilder.addParameter(variable, String.valueOf(execution.getVariable(variable)));
-//          }
-//      // execute request
-//      HttpUriRequest request = requestBuilder.build();
-//      HttpResponse response = client.execute(request);
-        
-        String id = execution.getProcessInstanceId();
-        String department = (String) execution.getVariable("department");
-        String title = (String) execution.getVariable("job_position");
-        String requirements = (String) execution.getVariable("requirements");
-        String description = (String) execution.getVariable("job_description");
-        String deadline = (String) execution.getVariable("date");
-//      String candidateProfile = (String) execution.getVariable("candidateProfile");
-        
-        String JSON = "{ \"department\": \""+ department +"\",";
-        JSON = JSON + "\"processId\": \""+ id +"\",";
-        JSON = JSON + "\"title\": \""+ title +"\",";
-        JSON = JSON + "\"requiredGraduation\": \""+ requirements +"\",";
-        JSON = JSON + "\"candidateProfile\": \""+ description +"\",";
-        JSON = JSON + "\"deadline\": \""+ deadline +"\"}";
-        
-        
-        String postURL = "http://";
-        try{
-        HttpPost post = new HttpPost(postURL);
-        StringEntity postString = new StringEntity(JSON);
-        
-        post.setHeader("content-type", "application/json");
-        post.setEntity(postString);
-        client.execute(post);
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-        
-    }
 }
