4c6a38d7800ccae4a94298a2e35c8154a2c5a27c
tetee <theresa-sollert@web.de>
2017-07-23 21:02:38 +0200

    refuseApplicant

--- src/main/java/org/camunda/wf/hiring/sendMessages/RefuseApplicant.java
+++ src/main/java/org/camunda/wf/hiring/sendMessages/RefuseApplicant.java
@@ -1,5 +1,24 @@
 package org.camunda.wf.hiring.sendMessages;
 
-public class RefuseApplicant {
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.camunda.wf.hiring.OutlookAccess.OutlookAccess;
 
+import microsoft.exchange.webservices.data.core.ExchangeService;
+
+public class RefuseApplicant implements JavaDelegate {
+
+	@Override
+	public void execute(DelegateExecution execution) throws Exception {
+
+		ExchangeService service = OutlookAccess.getOutlookAccess("HR_representive@outlook.de", "HRrepresentive");
+		String recipient = (String) execution.getVariable("cv.email");
+		String applicant = (String) execution.getVariable("cv.name");
+
+		String subject = "Refusal";
+		String message = "Hello, "+ applicant  +"\n We are very sorry but we have to refuse your applicaction. \n Best wishes, \n WBIG";
+
+		OutlookAccess.sendEmail(subject, message, recipient, service);
+
+	}
 }
